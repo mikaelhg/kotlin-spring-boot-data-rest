3edbc21b8e4e1e71590f18bf5407a1c1ce596501
Mikael Gueck <gumi@iki.fi>
2015-11-16 16:41:05 +0200

    Upgrade Kotlin.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 INSERT INTO restaurants (name, location, lat, lng) VALUES ('test', 'Helsinki', 123, 456);
 
-INSERT INTO menus (restaurant_id, `date`, `text`) VALUES (1, CURRENT_DATE(), 'Hello, World!');
+INSERT INTO menus (restaurant_id, `date`, title, `text`) VALUES (1, CURRENT_DATE(), 'Soups', 'Hello, World!');
