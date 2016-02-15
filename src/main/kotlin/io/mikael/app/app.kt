88e0b66d1677b39dfcccaf86ff88734afc2d47ca
Mikael Gueck <gumi@iki.fi>
2016-02-15 15:37:37 +0200

    Upgrade to Kotlin 1.0.0, implement suggestions from https://spring.io/blog/2016/02/15/developing-spring-boot-applications-with-kotlin

--- src/main/kotlin/io/mikael/app/app.kt
+++ src/main/kotlin/io/mikael/app/app.kt
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 open class Application {
 
-    companion object Runner {
-        @JvmStatic fun main(args: Array<String>) {
-            SpringApplication.run(Application::class.java, *args)
-        }
-    }
+}
 
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
 }
