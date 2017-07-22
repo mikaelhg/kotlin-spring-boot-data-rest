55dfad74a2764e2e02ae1e9906fbd6412a4cda9e
Mikael Gueck <gumi@iki.fi>
2017-07-22 16:09:33 +0300

    Upgrade, dox.

--- src/main/kotlin/io/mikael/app/app.kt
+++ src/main/kotlin/io/mikael/app/app.kt
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-open class Application {
-
-}
+open class Application
 
 @RestController
 class HelloController(val helloService: HelloService) {
